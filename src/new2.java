256c3b747419ec4a043b03a04ab89e708f86a864
chahar3085 <126415040+chahar3085@users.noreply.github.com>
2025-06-11 00:00:43 +0530

    Delete src directory

--- src/new2.java
+++
@@ -1,5 +0,0 @@
-public class new2 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/new3.java
+++
@@ -1,5 +0,0 @@
-public class new3 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Bridgelabz!");
-    }
-}
